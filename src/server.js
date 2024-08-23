e86fa0026ef2515b8a5b67e6115ac60f9bd7d688
MeliAviles <mel.avilesg@gmail.com>
2024-08-23 14:29:45 -0600

    review and request endpoints and models

--- src/server.js
+++ src/server.js
@@ -5,6 +5,9 @@ const cors = require("cors");
 
 const createPet = require("./controllers/createPet");
 const listPet = require("./controllers/listPet");
+const createReview = require("./controllers/createReview");
+const listRequest = require("./controllers/listRequest");
+const listReview = require("./controllers/listReview");
 const {createUser} = require("./controllers/createUser");
 
 const server = express(); //*creates server
@@ -42,7 +45,34 @@ server.get("/pets/", async (req, res) => {
 //*create user
 server.post("/users/", createUser)
 
+//* create review
+server.post("/reviews/", async (req, res) => {
+    try {
+        const newReview = await createReview(req.body);
+        res.status(200).json(newReview);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
 
+//* lists reviews
+server.get("/reviews/", async (req, res) => {
+    try {
+        const reviewList = await listReview(req.query);
+        res.status(200).json(reviewList);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
 
+//* lists requests
+router.get("/requests/", async (req, res) => {
+    try {
+        const requestList = await listRequest(req.query);
+        res.status(200).json(requestList);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
 
 module.exports = server; //*exports server
