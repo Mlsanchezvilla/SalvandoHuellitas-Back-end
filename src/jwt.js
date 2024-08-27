33ac1105c1ae50fc36c726071d94e89e593f6038
MeliAviles <mel.avilesg@gmail.com>
2024-08-26 22:24:29 -0600

    add name into jwt claims

--- src/jwt.js
+++ src/jwt.js
@@ -4,6 +4,7 @@ const jwt = require("jsonwebtoken");
 function createJWT (user){
     return jwt.sign({
         userID: user.id,
+        name: user.fullName,
         isAdmin: user.isAdmin,
     }, "secret-key") //ToDo change secret key and move to .env
 }
